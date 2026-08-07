0618ae25aee967006a7b7c01413b9d0172e47d22
lazizanajmiddinova795-beep <lazizanajmiddinova795@gmail.com>
2026-08-08 01:54:19 +0500

    fix: add favicon and manifest for correct web and PWA icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>FoodFlow - Restoran boshqaruv tizimi</title>
+        <link rel="icon" type="image/png" href="/favicon.png">
+        <link rel="manifest" href="/manifest.json">
 
         <!-- Google Fonts (Outfit) -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
